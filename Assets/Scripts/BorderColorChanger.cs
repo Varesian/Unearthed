5707fa97f709d90e841c78c32cf0291b2005018c
Cory Barr <cory.barr@gmail.com>
2012-12-31 15:02:40 -0800

    added blob scene, added border fading colors back, made particles last shorter

---
+++ Assets/Scripts/BorderColorChanger.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class BorderColorChanger : MonoBehaviour {
+	
+	public float transparencyChangeSpeed = 0.5f;
+	private Color initialColor;
+	private float currentAlpha;
+	private float transparencyDirection = 1.0f;
+	private float nextAlpha;
+	
+	// Use this for initialization
+	void Start () {
+		initialColor = renderer.material.color;
+		currentAlpha = renderer.material.color.a;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nextAlpha += Time.deltaTime * transparencyChangeSpeed * transparencyDirection;
+		if (nextAlpha > 1.0f) {
+			nextAlpha = 1.0f;
+			transparencyDirection = -1.0f;
+		} else if (nextAlpha < 0) {
+			nextAlpha = 0;
+			transparencyDirection = 1.0f;
+		}
+		renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, nextAlpha);
+	}
+}
