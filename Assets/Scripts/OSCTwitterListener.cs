11c407d349dd7d92f0347edc8d62532fc30e8d94
Cory Barr <cory.barr@gmail.com>
2012-12-31 17:19:41 -0800

    added Paul's code that makes fireworks shoot for certain tweets

--- Assets/Scripts/OSCTwitterListener.cs
+++ Assets/Scripts/OSCTwitterListener.cs
@@ -3,47 +3,30 @@ using System.Collections;
 
 public class OSCTwitterListener : OSCMessageListener {
 	
-	public GameObject targetObject;
-	int currColor = 0;
+	public ParticleEmitter emitter;
+	
+	bool emit = true;
 	
 	// Use this for initialization
 	protected override void Start () {
 		base.Start();
 	}
-	
-		// Update is called once per frame
-	void Update () {
-		
 
-	}
 	
 	protected override void Destroy() {
 		base.Destroy();
 	}
 	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Backslash)) {
+			emit = !emit;
+		}
+	}
 	
 	public override void OSCMessageReceiver(OscMessage message) {
 		
-		
-		switch (currColor) {
-		case 0:
-			targetObject.renderer.material.color = Color.blue;
-			break;
-		case 1:
-			targetObject.renderer.material.color = Color.red;
-			break;
-		case 2:
-			targetObject.renderer.material.color = Color.green;
-			break;
-		default:
-			targetObject.renderer.material.color = Color.blue;
-			break;
-		}
-		 
-		if (currColor == 2) {
-			currColor = 0;
-		} else {
-			currColor++;
+		if (emit) {
+			emitter.Emit();
 		}
 		
 		Debug.Log("Message...");
